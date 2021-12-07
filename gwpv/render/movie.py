8976e4c5ac81061e1202b99d2bf6a02bf8b69f94
damibabayemi <93312187+damibabayemi@users.noreply.github.com>
2021-12-07 10:25:18 +0100

    Update movie.py

--- gwpv/render/movie.py
+++ gwpv/render/movie.py
@@ -7,6 +7,6 @@ def render_movie(output_filename, frames_dir, frame_rate):
         'ffmpeg', '-vcodec', 'png', '-framerate',
         str(frame_rate), '-i',
         os.path.join(frames_dir, r'frame.%06d.png'), '-pix_fmt', 'yuv420p',
-        '-vcodec', 'libx264', '-crf', '17', '-threads', '0', '-preset', 'slow',
+        '-vcodec', 'mpeg4', '-threads', '0',
         '-y', output_filename + '.mp4'
     ])
