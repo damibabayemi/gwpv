cc08210645e852b8848681f86fe0b4e5b10b4cf7
damibabayemi <93312187+damibabayemi@users.noreply.github.com>
2021-12-20 15:07:08 +0100

    Update traj.py

--- gwpv/traj.py
+++ gwpv/traj.py
@@ -5,5 +5,7 @@ h5f = h5py.File('trajdata.h5', 'w')
 gr = h5f.create_group('BH1.dir')
 grid = np.zeros((100, 4))
 grid[:, 0] = np.linspace(0, 45, 100)
+coordcenter = grid
 grid[:, 3] = np.sin(np.linspace(0, 45, 100))
 gr.create_dataset('BH1', data=grid)
+gr.create_dataset('CoordCenterInertial.dat', data=coordcenter)
