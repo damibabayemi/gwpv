d5bc5e9751f4845074be5a2bdd72914290038584
damibabayemi <93312187+damibabayemi@users.noreply.github.com>
2022-01-16 16:33:05 +0100

    Update frames.py

--- gwpv/render/frames.py
+++ gwpv/render/frames.py
@@ -245,7 +245,7 @@ def render_frames(scene,
 
     # Display arbitrary text
     if 'Text' in scene:
-        text_annotation = pv.Shrink(pv.Text(**scene['Text']))
+        text_annotation = pv.Text(**scene['Text'])
         pv.Show(
             text_annotation, view, **scene['TextRepresentation'])
 
